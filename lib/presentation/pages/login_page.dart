dff945f48e54f41d4a685d98c6dd4b04ce29edaf
Olumide Awodeji <47766490+Cypher-O@users.noreply.github.com>
2024-08-27 08:11:54 +0100

    Added validation for textfield's on both auth pages

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -5,17 +5,18 @@ import 'package:to_do/presentation/pages/register_page.dart';
 import 'package:to_do/presentation/pages/todo_list_page.dart';
 
 class LoginPage extends StatelessWidget {
-  LoginPage({Key? key}) : super(key: key);
+  LoginPage({super.key});
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
-        centerTitle: true,
+        leading: null,
       ),
       body: BlocListener<AuthBloc, AuthState>(
         listener: (context, state) {
@@ -43,71 +44,97 @@ class LoginPage extends StatelessWidget {
               elevation: 5,
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 24),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => RegisterPage()),
-                            );
-                          },
-                          child: const Text('Register'),
-                        ),
-                        const SizedBox(width: 8),
-                        ElevatedButton(
-                          onPressed: () {
-                            context.read<AuthBloc>().add(
-                                  LoginRequested(
-                                    _emailController.text,
-                                    _passwordController.text,
-                                  ),
-                                );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 10,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                context.read<AuthBloc>().add(
+                                      LoginRequested(
+                                        _emailController.text,
+                                        _passwordController.text,
+                                      ),
+                                    );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegisterPage()),
+                          );
+                        },
+                        child: const Text(
+                          'Don\'t have an account? Register here',
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -117,85 +144,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
-
-// import 'dart:developer';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:to_do/presentation/bloc/auth/auth_bloc.dart';
-// import 'package:to_do/presentation/pages/register_page.dart';
-// import 'package:to_do/presentation/pages/todo_list_page.dart';
-
-// class LoginPage extends StatelessWidget {
-//   LoginPage({super.key});
-
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Login')),
-//       body: BlocListener<AuthBloc, AuthState>(
-//         listener: (context, state) {
-//           if (state is AuthSuccess) {
-//             // Navigate to home page
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               const SnackBar(content: Text('Login Successful')),
-//             );
-//             // Navigate to TodoListPage on successful login
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => const TodoListPage()),
-//             );
-//           } else if (state is AuthFailure) {
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               SnackBar(content: Text(state.error)),
-//             );
-//             log("Error occurred: ${state.error}");
-//           }
-//         },
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               TextField(
-//                 controller: _emailController,
-//                 decoration: const InputDecoration(labelText: 'Email'),
-//               ),
-//               TextField(
-//                 controller: _passwordController,
-//                 decoration: const InputDecoration(labelText: 'Password'),
-//                 obscureText: true,
-//               ),
-//               const SizedBox(height: 16),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   context.read<AuthBloc>().add(
-//                         LoginRequested(
-//                           _emailController.text,
-//                           _passwordController.text,
-//                         ),
-//                       );
-//                 },
-//                 child: const Text('Login'),
-//               ),
-//                TextButton(
-//                     onPressed: () {
-//                       Navigator.push(
-//                         context,
-//                         MaterialPageRoute(
-//                             builder: (context) => RegisterPage()),
-//                       );
-//                     },
-//                     child: const Text('Don\'t have an account? Register here'),
-//                   ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -5,18 +5,19 @@ import 'package:to_do/presentation/pages/login_page.dart';
 import 'package:to_do/presentation/pages/todo_list_page.dart';
 
 class RegisterPage extends StatelessWidget {
-  RegisterPage({Key? key}) : super(key: key);
+  RegisterPage({super.key});
 
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Register'),
-        centerTitle: true,
+        leading: null,
       ),
       body: BlocListener<AuthBloc, AuthState>(
         listener: (context, state) {
@@ -44,82 +45,112 @@ class RegisterPage extends StatelessWidget {
               elevation: 5,
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 24),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginPage()),
-                            );
-                          },
-                          child: const Text('Login'),
-                        ),
-                        const SizedBox(width: 8),
-                        ElevatedButton(
-                          onPressed: () {
-                            context.read<AuthBloc>().add(
-                                  RegisterRequested(
-                                    _usernameController.text,
-                                    _emailController.text,
-                                    _passwordController.text,
-                                  ),
-                                );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 10,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          child: const Text(
-                            'Register',
-                            style: TextStyle(color: Colors.white),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a username';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                context.read<AuthBloc>().add(
+                                      RegisterRequested(
+                                        _usernameController.text,
+                                        _emailController.text,
+                                        _passwordController.text,
+                                      ),
+                                    );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()),
+                          );
+                        },
+                        child: const Text(
+                          'Already have an account? Login',
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -129,89 +160,3 @@ class RegisterPage extends StatelessWidget {
     );
   }
 }
-
-// import 'dart:developer';
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:to_do/presentation/bloc/auth/auth_bloc.dart';
-// import 'package:to_do/presentation/pages/login_page.dart';
-// import 'package:to_do/presentation/pages/todo_list_page.dart';
-
-// class RegisterPage extends StatelessWidget {
-//   RegisterPage({super.key});
-
-//   final TextEditingController _usernameController = TextEditingController();
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Register')),
-//       body: BlocListener<AuthBloc, AuthState>(
-//         listener: (context, state) {
-//           if (state is AuthSuccess) {
-//             // Navigate to home page
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               const SnackBar(content: Text('Registration Successful')),
-//             );
-//             // Navigate to TodoListPage on successful login
-//             Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => const TodoListPage()),
-//             );
-//           } else if (state is AuthFailure) {
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               SnackBar(content: Text(state.error)),
-//             );
-//             log("Error occurred: ${state.error}");
-//           }
-//         },
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               TextField(
-//                 controller: _usernameController,
-//                 decoration: const InputDecoration(labelText: 'Username'),
-//               ),
-//               TextField(
-//                 controller: _emailController,
-//                 decoration: const InputDecoration(labelText: 'Email'),
-//               ),
-//               TextField(
-//                 controller: _passwordController,
-//                 decoration: const InputDecoration(labelText: 'Password'),
-//                 obscureText: true,
-//               ),
-//               const SizedBox(height: 16),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   context.read<AuthBloc>().add(
-//                         RegisterRequested(
-//                           _usernameController.text,
-//                           _emailController.text,
-//                           _passwordController.text,
-//                         ),
-//                       );
-//                 },
-//                 child: const Text('Register'),
-//               ),
-//               TextButton(
-//                 onPressed: () {
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(builder: (context) => LoginPage()),
-//                   );
-//                 },
-//                 child: const Text('Already have an account? Login'),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
